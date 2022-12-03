367a2b849e8bc5b9848a91c027212d6c2311c4a8
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-03 13:08:21 +0530

    Create hw.cpp

---
+++ hw.cpp
@@ -0,0 +1,9 @@
+// For printing statements in C++ programs, we use the cout statement.//
+//Purpose: To print Hello World!//
+//Code//
+
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Hello World!";
+}
