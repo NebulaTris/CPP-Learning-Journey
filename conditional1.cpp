bfac2395e0085c2f20529b572d25d72140c1cb6b
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-03 13:41:49 +0530

    Create conditional1.cpp

---
+++ conditional1.cpp
@@ -0,0 +1,15 @@
+/*Concept: Conditionals are used to execute a certain section of code only if some specific condition is fulfilled , and optionally execute other statements if the given condition is false. The result of given conditional expression must be either true or false
+There are different variations to do so:
+1)if statement
+2)if-else statement
+3)if-else-if statement 
+4)Nested if statement
+Code:  */
+
+
+
+/*while loop
+Loop statements allows us to execute a block of statements several number of times depending on certain condition while is one kind of loop that we can use.
+When executing, if the test expression result is true, then the actions inside the
+loop will be executed. This will continue as long as the expression result is true.
+*/
