6ef324f67d4be0d83ffcdd444d882469ea7aa1f1
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-03 13:18:19 +0530

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,13 @@
+/* Concept 1: For separating different lines in C++ , we use endl or \n.
+Concept 2: To take input from user we use the cin statement.
+  */
+
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,sum;
+  cin>>a;
+  cin>>b;
+  sum=a+b;
+  cout<<"Sum of two numbers is:"<<sum<<endl;
+}
